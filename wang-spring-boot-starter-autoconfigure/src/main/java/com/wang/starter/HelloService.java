2c2fb38cb0c7e30052bda3cea04ed50d3129018b
wAnG <228913299@qq.com>
2020-10-07 16:48:53 +0800

    自定义启动器

---
+++ wang-spring-boot-starter-autoconfigure/src/main/java/com/wang/starter/HelloService.java
@@ -0,0 +1,18 @@
+package com.wang.starter;
+
+public class HelloService {
+
+    HelloProperties helloProperties;
+
+    public HelloProperties getHelloProperties() {
+        return helloProperties;
+    }
+
+    public void setHelloProperties(HelloProperties helloProperties) {
+        this.helloProperties = helloProperties;
+    }
+
+    public String sayHelloWang(String name){
+        return helloProperties.getPrefix() + "-" + name + "-" + helloProperties.getSuffix();
+    }
+}
---
+++ wang-spring-boot-starter-autoconfigure/src/main/java/com/wang/starter/HelloServiceAutoConfiguration.java
@@ -0,0 +1,25 @@
+package com.wang.starter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.swing.*;
+
+@Configuration
+@ConditionalOnWebApplication //web应用生效
+@EnableConfigurationProperties(HelloProperties.class)
+public class HelloServiceAutoConfiguration {
+
+    @Autowired
+    HelloProperties helloProperties;
+
+    @Bean
+    public HelloService helloService(){
+        HelloService helloService = new HelloService();
+        helloService.setHelloProperties(helloProperties);
+        return helloService;
+    }
+}
