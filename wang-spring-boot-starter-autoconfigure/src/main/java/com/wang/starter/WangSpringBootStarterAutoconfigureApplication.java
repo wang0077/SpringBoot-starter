2dc46300609bd7e46c5db508eeb4f33e2e337e9b
wAnG <228913299@qq.com>
2020-10-07 16:28:02 +0800

    Initial commit

---
+++ wang-spring-boot-starter-autoconfigure/src/main/java/com/wang/starter/WangSpringBootStarterAutoconfigureApplication.java
@@ -0,0 +1,13 @@
+package com.wang.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WangSpringBootStarterAutoconfigureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WangSpringBootStarterAutoconfigureApplication.class, args);
+	}
+
+}
